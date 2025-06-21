8ff146b6b3e4d09e0ae26ea8066cb94a650d18ff
suman <sumansouravrs002@gmail.com>
2025-06-21 18:58:27 +0530

    Initial commit

--- server/models/Poll.js
+++ server/models/Poll.js
@@ -8,6 +8,24 @@ const pollSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});const mongoose = require('mongoose');
+
+const pollSchema = new mongoose.Schema({
+  question: String,
+  options: [String],
+  responses: {
+    type: Map,
+    of: Number,
+    default: {},
+  },
+  isActive: Boolean,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = mongoose.model('Poll', pollSchema);
+
 
 module.exports = mongoose.model('Poll', pollSchema);
